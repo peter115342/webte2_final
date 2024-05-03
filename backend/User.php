7bdaf482137e398f1bc809f15d4166106b80fdf1
filip <xbercikf@stuba.sk>
2024-05-03 21:06:37 +0200

    created api, Question, User

---
+++ backend/User.php
@@ -0,0 +1,93 @@
+<?php
+
+class User
+{
+
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function getAllUsers()
+    {
+        /* GET ALL USERS + QUESTIONS + ANSWERS
+        $query = "SELECT users.username, users.password, users.administrator, questions.date, 
+        questions.user_id , questions.subject , questions.question , questions.type_id, questions.code, 
+        questions.qr_code, answers.question_id, answers.answer, answers.correct, answers.count, types.type
+        FROM users 
+        LEFT JOIN questions ON users.id = questions.user_id  
+        LEFT JOIN answers ON answers.question_id = questions.id
+        LEFT JOIN types ON answers.type_id = types.id";
+        */
+        $query = "SELECT username, administrator FROM users";
+        $result = mysqli_query($this->conn, $query);
+        $users = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $users[] = $row;
+        }
+        return $users;
+    }
+
+    public function getUser($data)
+    {
+        $username = $data['username'];
+        $password = $data['password'];
+        $query = "SELECT username, administrator FROM users
+              WHERE users.username = ? AND users.password = ?";
+    
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+        $stmt->close();
+        return $user;
+    }
+
+
+    public function createUser($data)
+    {
+        $username = $data['username'];
+        $password = $data['password'];
+        $stmt = $this->conn->prepare("SELECT id FROM users WHERE username = ? AND password = ?");
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return false;
+        }
+        $stmt = $this->conn->prepare("INSERT INTO users (username, password, administrator) VALUES (?, ?, 0)");
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return true;
+    }
+
+    public function updateUser($id, $data)
+    {
+        $password = $data['password'];
+        $stmt = $this->conn->prepare("UPDATE users SET password = ? WHERE id = ?");
+        $stmt->bind_param("si", $password, $id);
+        $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            $stmt->close();
+            return true;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
+
+    public function deleteUser($id)
+    {    
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $affectedRows = $stmt->affected_rows;
+        $stmt->close();
+        return $affectedRows > 0;
+    }   
+}
---
+++ backend/api.php
@@ -0,0 +1,199 @@
+<?php
+
+require_once "User.php";
+require_once "Question.php";
+require_once "config.php";
+
+$userObject = new User($conn);
+$questionObject = new Question($conn);
+
+$method = $_SERVER['REQUEST_METHOD'];
+$queryString = $_SERVER['QUERY_STRING'];
+parse_str($queryString, $params);
+$endpoint = $params['endpoint'];
+
+
+$endpoint = '/' . $endpoint;
+
+header('Content-Type: application/json');
+switch ($method) {
+    case 'GET':
+        // GET USER
+        if ($endpoint ===  "/login") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (empty($data)) {
+                http_response_code(400);
+                echo json_encode(["message" => "Request body is empty or blank"]);
+                exit(); 
+            }            
+            $user = $userObject->getUser($data);
+            http_response_code(200);
+            echo json_encode($user);
+        }  
+        // GET ALL USERS
+        elseif ($endpoint ===  "/users") {
+           $users = $userObject->getAllUsers();
+           http_response_code(200);
+           echo json_encode($users);
+        }  
+        // GET ALL QUESTIONS BY USER
+        elseif (preg_match("/^\/userQuestions\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $questions = $questionObject->getQuestionsByUserId($id);
+            http_response_code(200);
+            echo json_encode($questions);
+        }
+        // QUESTION BY ID
+        elseif (preg_match("/^\/question\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $question = $questionObject->getQuestionById($id);
+            http_response_code(200);
+            echo json_encode($question);
+        }
+        
+        // QUESTION BY CODE
+        elseif (preg_match("/^\/([a-zA-Z0-9]{5})$/", $endpoint, $matches)) {
+            $code = $matches[1]; 
+            $question = $questionObject->getQuestionByCode($code);
+            http_response_code(200);
+            echo json_encode($question);
+        } 
+        else {
+            http_response_code(400);
+            echo json_encode(["message" => "Bad request"]);
+        }
+        break;
+    case 'POST':
+        // CREATE ANSWER 
+        if ($endpoint === "/answer") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $questionObject->createAnswer($data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        } 
+        // CREATE QUESTION
+        elseif ($endpoint === "/question") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $questionObject->createQuestion($data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        } 
+        // CREATE USER
+        elseif ($endpoint === "/user") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $userObject->createUser($data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["message" => "Bad request"]);
+        }
+        break;
+    case 'PUT':
+        // UPDATE ANSWER
+        if (preg_match("/^\/answer\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $questionObject->updateAnswer($id, $data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        } 
+        // UPDATE QUESTION 
+        elseif (preg_match("/^\/question\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $questionObject->updateQuestion($id, $data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        }
+        // UPDATE USER
+        elseif (preg_match("/^\/user\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $result = $userObject->updateUser($id, $data);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        }
+        else {
+            http_response_code(400);
+            echo json_encode(["message" => "Bad request"]);
+        }
+        break;
+    case 'DELETE':
+        // DELETE ANSWER
+        if (preg_match("/^\/answer\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $result = $questionObject->deleteAnswer($id);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        } 
+        // DELETE QUESTION
+        elseif (preg_match("/^\/question\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $result = $questionObject->deleteQuestion($id);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        }
+        // DELETE USER
+        elseif (preg_match("/^\/user\/(\d+)$/", $endpoint, $matches)) {
+            $id = $matches[1];
+            $result = $userObject->deleteUser($id);
+            if($result){
+                echo json_encode(["message" => "OK"]);
+                http_response_code(200);
+            } else {
+                echo json_encode(["message" => "Error"]);
+                http_response_code(400);
+            }
+        }
+        else {
+            http_response_code(400);
+            echo json_encode(["message" => "Bad request"]);
+        }
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(["message" => "Method not allowed"]);
+
+        
+    }
