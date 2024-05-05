0a1857c4035e16f8009346b69834bba10e4cdf8e
peter115342 <pmuzslay8@gmail.com>
2024-05-05 22:25:57 +0200

    added vue-i18n, not yet bilingual

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="primary">
       <v-toolbar-title class="title">PresentFlow</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn v-if="!isLoggedIn" @click="login">Login</v-btn>
+      <v-btn v-if="!isLoggedIn" @click="login">{{ $t('login') }}</v-btn>
       <v-icon v-if="isLoggedIn">mdi-account-circle</v-icon>
       <v-btn icon @click="toggleLanguage">
         <v-img :src="currentFlag" alt="Language Flag" width="24" height="24" />
@@ -18,27 +18,26 @@
 
 <script setup>
 import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n(); // useI18n should be called within the setup function
 
 const isLoggedIn = ref(false);
 const login = () => {
   isLoggedIn.value = true;
 };
 
-const currentLanguage = ref('en'); // Default language is English
 let englishFlagUrl = '';
 let slovakFlagUrl = '';
 
 const toggleLanguage = async () => {
-  // Toggle between English and Slovak
-  currentLanguage.value = currentLanguage.value === 'en' ? 'sk' : 'en';
+  locale.value = locale.value === 'en' ? 'sk' : 'en';
   // Load the flag images
-  if (currentLanguage.value === 'en') {
-    englishFlagUrl = (await import('./assets/united-kingdom.png')).default;
+  if (locale.value === 'en') {
+    englishFlagUrl = (await import('@/assets/united-kingdom.png')).default;
   } else {
-    slovakFlagUrl = (await import('./assets/slovakia.png')).default;
+    slovakFlagUrl = (await import('@/assets/slovakia.png')).default;
   }
-  // Set the initial flag URL based on the current language
-  currentFlag.value = currentLanguage.value === 'en' ? englishFlagUrl : slovakFlagUrl;
 };
 
 // Initial loading of flag images and setting the initial flag URL
@@ -47,8 +46,8 @@ toggleLanguage();
 // Computed property to dynamically get the current flag URL
 const currentFlag = ref('');
 import { watch } from 'vue';
-watch(currentLanguage, () => {
-  currentFlag.value = currentLanguage.value === 'en' ? englishFlagUrl : slovakFlagUrl;
+watch(locale, () => {
+  currentFlag.value = locale.value === 'en' ? englishFlagUrl : slovakFlagUrl;
 });
 
 </script>
