1c99c7e3f1e524b8b201f1b90d5e9d482defeab6
peter115342 <pmuzslay8@gmail.com>
2024-05-10 11:05:58 +0200

    fixed login  now works through cookies, added frontend validations. Missing backend validations

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@
     <v-app-bar app color="primary">
       <v-toolbar-title class="title">{{ $t('appTitle') }}</v-toolbar-title>
       <v-spacer></v-spacer>
-      <template v-if="!isLoggedIn">
+      <template v-if="!hasAccessToken">
         <v-btn @click="showLoginForm = true" class="login-button elevation-2">{{ $t('login') }}</v-btn>
         <v-dialog v-model="showLoginForm" max-width="500">
           <template v-slot:activator="{ on }"></template>
           <v-card>
             <v-card-text>
-              <Login @loginSuccess="handleLoginSuccess" @loginError="handleLoginError" />
+              <!-- Login component emits accessToken -->
+              <Login @loginSuccess="handleLoginSuccess" @registerSuccess="handleRegisterSuccess" @loginError="handleLoginError" @accessToken="handleAccessToken" @username = "saveUsername" />
             </v-card-text>
           </v-card>
         </v-dialog>
@@ -18,12 +19,13 @@
         <v-btn icon @click="showUserModal = !showUserModal">
           <v-icon>mdi-account-circle</v-icon>
         </v-btn>
-        <v-dialog v-model="showUserModal" max-width="300" persistent @click:outside="closeUserModal">
+        <v-dialog v-model="showUserModal" max-width="300"  persistent @click:outside="closeUserModal" content-class="user-modal">
           <template v-slot:activator="{ on }"></template>
           <v-card>
-            <v-card-title>{{ $t('loggedInAs') }} {{ loggedInUser }}</v-card-title>
+            <v-card-title>{{ $t('loggedInAs') }} {{ getUsernameFromLocalStorage()}}</v-card-title>
             <v-card-actions>
-              <v-btn class="logout-button" @click="logout">{{ $t('logout') }}</v-btn>            </v-card-actions>
+              <v-btn    class="logout-button" @click="logout">{{ $t('logout') }}</v-btn>
+            </v-card-actions>
           </v-card>
         </v-dialog>
       </template>
@@ -39,28 +41,48 @@
 </template>
 
 <script setup>
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch, onMounted, computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Login from './components/Login.vue';
-import { useAppStore } from './store/app';
 
-const { locale } = useI18n(); 
-const store = useAppStore();
+const { locale } = useI18n();
 
-const isLoggedIn = ref(document.cookie.includes('isLoggedIn=true'));
+// Define reactive variables
 const showLoginForm = ref(false);
 const showUserModal = ref(false);
+const cookieValue = ref(document.cookie);
 
-const loggedInUser = ref('');
-
+// Handle successful login
 const handleLoginSuccess = () => {
-  isLoggedIn.value = true;
   showLoginForm.value = false;
-  document.cookie = 'isLoggedIn=true; expires=Thu, 01 Jan 2026 00:00:00 UTC; path=/;';
-};
+  handleAccessToken(cookieValue.value.split('=')[1]);
+  window.location.reload();
 
+};
+const saveUsername = (username) => {
+  localStorage.setItem('username', username);
+};
+const getUsernameFromLocalStorage = () => {
+  return localStorage.getItem('username');
+};
+// Handle login error
 const handleLoginError = () => {
   // Handle login error, show message or take appropriate action
+};
+
+// Handle successful user registration
+const handleRegisterSuccess = () => {
+  setTimeout(() => {
+    showLoginForm.value = false;
+  }, 2500);
+};
+
+// Handle received access token
+const handleAccessToken = (accessToken) => {
+  if (isValidAccessToken(accessToken)) {
+    // Set the access token cookie
+    document.cookie = `access_token=${accessToken}; path=/;`;
+  }
 };
 
 // Function to toggle between languages
@@ -72,42 +94,36 @@ const toggleLanguage = async () => {
 import skFlag from './assets/slovakia.png';
 import enFlag from './assets/united-kingdom.png';
 
-let currentFlag = ref('');
+// Define current language flag
+const currentFlag = ref('');
 watch(locale, () => {
   currentFlag.value = locale.value === 'en' ? enFlag : skFlag;
-}); // Added closing bracket here
+});
+toggleLanguage(); // Toggle language on startup
 
-// Function to handle logout
-const logout = () => {
-  isLoggedIn.value = false;
-  // Clear cookie to indicate user is logged out
-  document.cookie = 'isLoggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-};
-
-toggleLanguage();
-
-// Close the user modal when clicked outside of it
+// Close the user modal when clicked outside
 const closeUserModal = () => {
   showUserModal.value = false;
 };
 
-// Watch for changes in login status and access token
-watch(() => [isLoggedIn.value, store.accessToken], ([newValue, accessToken]) => {
-  if (newValue && accessToken) {
-    // Check if access token is valid (replace with your validation logic)
-    if (!isValidAccessToken(accessToken)) {
-      logout();
-    }
-  }
+// Watch for changes in document.cookie
+watch(() => document.cookie, (newValue) => {
+  cookieValue.value = newValue;
 });
 
-// Function to validate access token (Replace this with your actual validation logic)
-const isValidAccessToken = (accessToken) => {
-  // Example validation logic
-  return accessToken.length === 32 && /^[a-z0-9]+$/i.test(accessToken);
+// Check if the access token is valid
+const isValidAccessToken = (access_token) => {
+  return access_token && /^[a-zA-Z0-9]{32}$/.test(access_token);
 };
 
-// Add click event listener to close modal when clicked outside of it
+// Logout function
+const logout = () => {
+  document.cookie = 'access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+  localStorage.removeItem('username');
+  window.location.reload();
+};
+
+// Close user modal when mounted
 onMounted(() => {
   document.body.addEventListener('click', (event) => {
     const modal = document.querySelector('.v-dialog__content--active');
@@ -117,6 +133,9 @@ onMounted(() => {
   });
 });
 
+const hasAccessToken = computed(() => {
+  return isValidAccessToken(cookieValue.value.split('=')[1]);
+});
 </script>
 
 <style scoped>
@@ -125,8 +144,16 @@ onMounted(() => {
 }
 .logout-button {
   background-color: #f9de82;
+  margin: 0 auto;
 }
 .login-button {
   background-color: #ffd166;
+}
+
+.user-modal .v-dialog {
+  position: absolute !important ;
+  top: 0 !important;
+  right: 0;
+  z-index: 1000;
 }
 </style>
