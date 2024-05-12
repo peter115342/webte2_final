71d46d7907293c86cec162d4e39907d4dedda78b
RadovanBorsig <radovan.borsig@gmailc.om>
2024-05-12 16:54:59 +0200

    update getPDF

--- backend/getPDF.php
+++ backend/getPDF.php
@@ -2,7 +2,7 @@
 
 require_once('TCPDF/tcpdf.php');
 
-$url = '';
+$url = 'https://node22.webte.fei.stuba.sk/Manual/index.php';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -21,11 +21,46 @@ $pdf->SetHeaderData('', 0, 'PDF Manual', '');
 $pdf->setPrintHeader(false);
 $pdf->setPrintFooter(false);
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
 $pdf->AddPage();
 
-$pdf->SetFont('times', '', 10);
 
-$pdf->writeHTML($response, true, false, true, false, '');
+$dom = new DOMDocument();
+$dom->loadHTML($response);
+
+$title = $dom->getElementsByTagName('h1')->item(0);
+$pdf->SetFont('freeserif', 'B', 14);
+$pdf->writeHTML($title->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle1 = $dom->getElementsByTagName('h3')->item(0);
+$pdf->SetFont('freeserif', 'B', 12);
+$pdf->writeHTML($subtitle1->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle1_text = $dom->getElementsByTagName('p')->item(0);
+$pdf->SetFont('freeserif', '', 10);
+$pdf->writeHTML($subtitle1_text->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle2 = $dom->getElementsByTagName('h3')->item(1);
+$pdf->SetFont('freeserif', 'B', 12);
+$pdf->writeHTML($subtitle2->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle2_text = $dom->getElementsByTagName('p')->item(1);
+$pdf->SetFont('freeserif', '', 10);
+$pdf->writeHTML($subtitle2_text->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle3 = $dom->getElementsByTagName('h3')->item(2);
+$pdf->SetFont('freeserif', 'B', 12);
+$pdf->writeHTML($subtitle3->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
+
+$subtitle3_text = $dom->getElementsByTagName('p')->item(2);
+$pdf->SetFont('freeserif', '', 10);
+$pdf->writeHTML($subtitle3_text->textContent, true, false, true);
+$pdf->writeHTML('<br>', true, false, true);
 
 $pdf->Output('file.pdf', 'D');
+
